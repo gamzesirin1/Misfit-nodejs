08ee90553d7d1e39f0e23687b239deb80096df23
Gamze Şirin <117711013+Gamzesrn@users.noreply.github.com>
2023-09-19 13:28:46 +0300

    workspace created.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+	res.status(200).send('index sayfası')
+})
+
+const port = 3000
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`)
+})
