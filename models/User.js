38647e5b80a842970613ee8888758a7c560f79c4
Gamze Şirin <117711013+Gamzesrn@users.noreply.github.com>
2023-09-20 19:46:50 +0300

    edit project.

--- models/User.js
+++ models/User.js
@@ -14,6 +14,11 @@ const UserSchema = new Schema({
 	password: {
 		type: String,
 		required: true
+	},
+	role: {
+		type: String,
+		enum: ['student', 'teacher', 'admin'],
+		default: 'student'
 	}
 })
 
--- routes/trainingRoute.js
+++ routes/trainingRoute.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const trainingController = require('../controllers/trainingController')
+const roleMiddleware = require('../middlewares/roleMiddleware')
 const router = express.Router()
-router.route('/').post(trainingController.createTraining)
+router.route('/').post(roleMiddleware(['teacher', 'admin']), trainingController.createTraining)
 router.route('/').get(trainingController.getAllTrainings)
 router.route('/:slug').get(trainingController.getTraining)
 
